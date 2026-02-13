ef68a20d1436c09e2c3af57f68689b293047fd38
Stanislav Shopov <stasio4777@gmail.com>
2026-02-13 19:21:39 +0200

    add vite proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +9,14 @@ export default defineConfig({
       },
     }),
   ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://fusion-shop.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
